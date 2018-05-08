b62246194fce92acb6532157bf5b92295b7653e3
RichterOneZero <richter@outlook.co.nz>
2018-05-08 17:53:00 +1200

    Finished game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody2D rb2d;
 	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
 
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		setCountText();
+		winText.text = "";
 	}
 
-	void FixedUpdate () {
+	void FixedUpdate () {	//This event triggers after everything else, including physics!
 		float moveHorizonal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector2 movement = new Vector2 (moveHorizonal, moveVertical);
@@ -21,6 +28,15 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.gameObject.CompareTag ("PickUp")) {
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			setCountText();
+		}
+	}
+
+	void setCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 6) {
+			winText.text = "YOU WIN!!";
 		}
 	}
 
